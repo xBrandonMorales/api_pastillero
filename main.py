eed26958852b6bb81006dc0e961cefa5b0dff598
Brandon Castro Morales <adrimora.0232@gmail.com>
2024-12-03 22:28:11 +0000

    hhhhh

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ from typing import List
 app = FastAPI()
 
 # Configura la conexión a MongoDB Atlas
-MONGO_URL = MONGO_URL = "mongodb+srv://1722110607:1234@cluster0.o6f1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
+MONGO_URL = "mongodb+srv://1722110607:1234@cluster0.o6f1d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = AsyncIOMotorClient(MONGO_URL)
 db = client.pastillero  # Reemplaza "proyecto" con el nombre de tu base de datos si es diferente
@@ -29,32 +28,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/pastillas/", response_model=List[Pastilla], summary="Obtener todas las pastillas", description="Devuelve una lista de todas las pastillas en la base de datos.")
+@app.get(
+    "/pastillas/",
+    response_model=List[Pastilla],
+    summary="Obtener todas las pastillas",
+    description="Devuelve una lista de todas las pastillas en la base de datos.",
+)
 async def get_pastillas():
     pastillas = await db.pastillas.find().to_list(None)
     return pastillas
 
-@app.post("/pastillas/", response_model=Pastilla, summary="Agregar una nueva pastilla", description="Agrega una nueva pastilla con su información.")
+@app.post(
+    "/pastillas/",
+    response_model=Pastilla,
+    summary="Agregar una nueva pastilla",
+    description="Agrega una nueva pastilla con su información.",
+)
 async def create_pastilla(pastilla: Pastilla):
     result = await db.pastillas.insert_one(pastilla.dict())
     if result.inserted_id:
         return pastilla
     raise HTTPException(status_code=500, detail="Error al agregar la pastilla.")
 
-@app.put("/pastillas/{nombre}", summary="Actualizar una pastilla", description="Actualiza los datos de una pastilla existente.")
+@app.put(
+    "/pastillas/{nombre}",
+    summary="Actualizar una pastilla",
+    description="Actualiza los datos de una pastilla existente.",
+)
 async def update_pastilla(nombre: str, pastilla: Pastilla):
     result = await db.pastillas.update_one({"nombre": nombre}, {"$set": pastilla.dict()})
     if result.modified_count == 1:
         return {"msg": "Pastilla actualizada correctamente."}
     raise HTTPException(status_code=404, detail="Pastilla no encontrada.")
 
-@app.delete("/pastillas/{nombre}", summary="Eliminar una pastilla", description="Elimina una pastilla por su nombre.")
+@app.delete(
+    "/pastillas/{nombre}",
+    summary="Eliminar una pastilla",
+    description="Elimina una pastilla por su nombre.",
+)
 async def delete_pastilla(nombre: str):
     result = await db.pastillas.delete_one({"nombre": nombre})
     if result.deleted_count == 1:
         return {"msg": "Pastilla eliminada correctamente."}
     raise HTTPException(status_code=404, detail="Pastilla no encontrada.")
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app)
